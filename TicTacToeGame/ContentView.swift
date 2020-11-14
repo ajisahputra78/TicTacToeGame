a311609cf5e18ff13baaf660b790fe001f4aa5d8
ajisahputra78 <ajisaputro.s.kom@gmail.com>
2020-11-15 00:45:00 +0700

    Initial Commit

---
+++ TicTacToeGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToeGame
+//
+//  Created by Aji_sahputra on 15/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
